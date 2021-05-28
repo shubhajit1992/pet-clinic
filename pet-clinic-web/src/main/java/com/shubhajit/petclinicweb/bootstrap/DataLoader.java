49933a1f0bcc636c1da0974d96482c7c02ecc3c3
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-28 07:44:07 +0530

    adding spring config

--- pet-clinic-web/src/main/java/com/shubhajit/petclinicweb/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/shubhajit/petclinicweb/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.shubhajit.petclinicweb.entities.Owner;
 import com.shubhajit.petclinicweb.entities.Vet;
 import com.shubhajit.petclinicweb.services.OwnerService;
 import com.shubhajit.petclinicweb.services.VetService;
-import com.shubhajit.petclinicweb.services.map.OwnerServiceMap;
-import com.shubhajit.petclinicweb.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +12,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
