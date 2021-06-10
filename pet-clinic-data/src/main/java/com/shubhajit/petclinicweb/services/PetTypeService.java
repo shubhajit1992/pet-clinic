9240a8a305bd9e379390a7141659d96fd817bc7f
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-06-10 20:30:39 +0530

    Added PetTypeServiceMap Impl. Closes #35.

---
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.shubhajit.petclinicweb.services;
+
+import com.shubhajit.petclinicweb.entities.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/services/map/PetTypeServiceMap.java
@@ -0,0 +1,33 @@
+package com.shubhajit.petclinicweb.services.map;
+
+import com.shubhajit.petclinicweb.entities.PetType;
+import com.shubhajit.petclinicweb.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
