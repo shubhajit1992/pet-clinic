19f0fffb14250c4091ab3d6244563b483a396992
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-06-10 20:40:41 +0530

    Added PetTypes in bootstrap. Closes #38.

--- pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/services/map/PetTypeServiceMap.java
@@ -2,9 +2,11 @@ package com.shubhajit.petclinicweb.services.map;
 
 import com.shubhajit.petclinicweb.entities.PetType;
 import com.shubhajit.petclinicweb.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
     public PetType findById(Long id) {
--- pet-clinic-web/src/main/java/com/shubhajit/petclinicweb/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/shubhajit/petclinicweb/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.shubhajit.petclinicweb.bootstrap;
 
 import com.shubhajit.petclinicweb.entities.Owner;
+import com.shubhajit.petclinicweb.entities.PetType;
 import com.shubhajit.petclinicweb.entities.Vet;
 import com.shubhajit.petclinicweb.services.OwnerService;
+import com.shubhajit.petclinicweb.services.PetTypeService;
 import com.shubhajit.petclinicweb.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,24 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
