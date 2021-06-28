8177ea2191082cc00ef285128e3e612dc6e51004
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-06-29 03:20:09 +0530

    Added VisitServiceMap implementation. Closes #52.

---
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.shubhajit.petclinicweb.services;
+
+import com.shubhajit.petclinicweb.entities.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/services/map/VisitServiceMap.java
@@ -0,0 +1,37 @@
+package com.shubhajit.petclinicweb.services.map;
+
+import com.shubhajit.petclinicweb.entities.Visit;
+import com.shubhajit.petclinicweb.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit.");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
