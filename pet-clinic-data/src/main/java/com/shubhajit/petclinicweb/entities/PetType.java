288e19deca887e428907634944f5bae0a36eea8f
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-06-29 01:26:45 +0530

    updated Owner with JPA mapping entities. Closes #42.

--- pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/entities/PetType.java
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/entities/PetType.java
@@ -1,6 +1,14 @@
 package com.shubhajit.petclinicweb.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_types")
 public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
