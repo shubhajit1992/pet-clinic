9cc38feb24089aa9a5ccf869fadc7b2c969b56b7
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-06-30 10:05:51 +0530

    Added Project Lombok. Closes #57.

--- pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/entities/PetType.java
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/entities/PetType.java
@@ -1,9 +1,20 @@
 package com.shubhajit.petclinicweb.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "pet_types")
 public class PetType extends BaseEntity {
@@ -11,11 +22,4 @@ public class PetType extends BaseEntity {
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/entities/Visit.java
+++ pet-clinic-data/src/main/java/com/shubhajit/petclinicweb/entities/Visit.java
@@ -1,5 +1,11 @@
 package com.shubhajit.petclinicweb.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
 import javax.persistence.Column;
@@ -8,6 +14,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -22,27 +33,4 @@ public class Visit extends BaseEntity {
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
